ad573dd1ff5f4048bba5b6154ac38f564a2b1988
Amanda <achappell@me.com>
2026-03-22 11:30:28 -0500

    Initial Commit

---
+++ Family Hub/Family_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_HubApp.swift
+//  Family Hub
+//
+//  Created by Amanda Chappell on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Family_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Family HubTests/Family_HubTests.swift
@@ -0,0 +1,16 @@
+//
+//  Family_HubTests.swift
+//  Family HubTests
+//
+//  Created by Amanda Chappell on 3/22/26.
+//
+
+import Testing
+
+struct Family_HubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
